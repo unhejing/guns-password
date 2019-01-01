9d0aab6341ef66917fc6bc48e5cc4cf78ce12613
“unhejing” <1054214757@qq.com>
2019-01-01 14:52:16 +0800

    接口修改

--- guns-admin/src/main/java/com/stylefeng/guns/modular/api/ApiController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/api/ApiController.java
@@ -1,13 +1,22 @@
 package com.stylefeng.guns.modular.api;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.google.common.collect.Lists;
 import com.stylefeng.guns.core.base.controller.BaseController;
 import com.stylefeng.guns.core.base.tips.ErrorTip;
 import com.stylefeng.guns.core.common.model.JSONResult;
 import com.stylefeng.guns.core.shiro.ShiroKit;
 import com.stylefeng.guns.core.shiro.ShiroUser;
 import com.stylefeng.guns.core.util.JwtTokenUtil;
+import com.stylefeng.guns.modular.billingDetail.service.IBillingDetailService;
+import com.stylefeng.guns.modular.billingDetail.vo.BillingDetailVo;
+import com.stylefeng.guns.modular.password.service.IBizPasswordService;
+import com.stylefeng.guns.modular.system.dao.BizPasswordMapper;
 import com.stylefeng.guns.modular.system.dao.CustomerMapper;
 import com.stylefeng.guns.modular.system.dao.UserMapper;
+import com.stylefeng.guns.modular.system.model.BillingDetail;
+import com.stylefeng.guns.modular.system.model.BizPassword;
 import com.stylefeng.guns.modular.system.model.Customer;
 import com.stylefeng.guns.modular.system.model.User;
 import io.swagger.annotations.ApiOperation;
@@ -17,10 +26,14 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.crypto.hash.Md5Hash;
 import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * 接口控制器提供
@@ -33,17 +46,26 @@ import java.util.HashMap;
 public class ApiController extends BaseController {
 
     @Autowired
+    private IBizPasswordService bizPasswordService;
+
+    @Autowired
     private UserMapper userMapper;
 
     @Autowired
     private CustomerMapper customerMapper;
+
+    @Autowired
+    private BizPasswordMapper passwordMapper;
+
+    @Autowired
+    private IBillingDetailService billingDetailService;
 
     /**
      * api登录接口，通过账号密码获取token
      */
     @ApiOperation("登陆接口")
     @PostMapping("/auth")
-    public Object auth(@RequestParam("username") String username,
+    public JSONResult auth(@RequestParam("username") String username,
                        @RequestParam("password") String password) {
 
         //封装请求账号密码为shiro可验证的token
@@ -66,12 +88,39 @@ public class ApiController extends BaseController {
                 usernamePasswordToken, simpleAuthenticationInfo);
 
         if (passwordTrueFlag) {
-            HashMap<String, Object> result = new HashMap<>();
-            result.put("token", JwtTokenUtil.generateToken(String.valueOf(user.getId())));
-            return result;
+            HashMap<String, Object> data = new HashMap<>();
+            data.put("token", JwtTokenUtil.generateToken(String.valueOf(user.getId())));
+            return new JSONResult(JSONResult.CODE_SUCCESS,data);
         } else {
-            return new ErrorTip(500, "账号密码错误！");
+            return new JSONResult(JSONResult.CODE_FAIL,"账号密码错误");
         }
+    }
+
+    @ApiOperation("APP注册")
+    @PostMapping("/register")
+    public JSONResult register(@RequestParam("username") String username,
+                               @RequestParam("password") String password) {
+
+        if(StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
+            return new JSONResult(JSONResult.CODE_FAIL,"用户名或密码为空！");
+        }
+        JSONResult result;
+        //获取数据库中的账号密码，准备比对
+        Customer customer = customerMapper.getByAccount(username);
+        if(customer != null){
+            return new JSONResult(JSONResult.CODE_FAIL,"该账号已存在！");
+        }
+        customer = new Customer();
+        customer.setUsername(username);
+        customer.setPassword(ShiroKit.md5(password, "unhejing"));
+        customer.setCreateTime(new Date());
+        customerMapper.insert(customer);
+        customer.setPassword("");
+        result = new JSONResult(JSONResult.CODE_SUCCESS,"注册成功",customer);
+
+        //校验用户账号密码
+        return result;
+
     }
 
     /**
@@ -82,6 +131,87 @@ public class ApiController extends BaseController {
     public Object test() {
 
         return SUCCESS_TIP;
+    }
+
+    @ApiOperation("获取密码列表")
+    @PostMapping("getPasswordList")
+    public JSONResult getPasswordList(@RequestParam("userId") Integer userId){
+        JSONResult result = new JSONResult();
+        if(Objects.isNull(userId)){
+            return new JSONResult(JSONResult.CODE_FAIL,"用户ueserId为空！");
+        }
+        // 通过usrId查询出password列表
+        Wrapper<BizPassword> wrapper = new EntityWrapper<>();
+        wrapper = wrapper.eq("user_id", userId);
+        List<BizPassword> passwordList = passwordMapper.selectList(wrapper);
+        result = new JSONResult(JSONResult.CODE_SUCCESS,"获取密码列表成功",passwordList);
+        return result;
+    }
+
+    @ApiOperation("添加密码")
+    @PostMapping("addPassword")
+    public JSONResult addPassword(@RequestParam("userId") Integer userId,@RequestParam("username") String username,@RequestParam("password") String password){
+        JSONResult result;
+        if(Objects.isNull(userId)){
+            return new JSONResult(JSONResult.CODE_FAIL,"用户ueserId为空！");
+        }
+        if(StringUtils.isBlank(username) || StringUtils.isBlank(password)){
+            return new JSONResult(JSONResult.CODE_FAIL,"用户名或密码为空！");
+        }
+        // 通过usrId查询出password列表
+        BizPassword bizPassword = new BizPassword();
+        bizPassword.setUsername(username);
+        bizPassword.setPassword(password);
+        bizPassword.setUserId(userId);
+        bizPassword.setCreateTime(new Date());
+
+        passwordMapper.insert(bizPassword);
+        result = new JSONResult(JSONResult.CODE_SUCCESS,"添加成功！",bizPassword);
+        return result;
+    }
+
+    @ApiOperation("分页获取密码列表")
+    @RequestMapping(value = "/findPasswordByPage", method = RequestMethod.POST)
+    public Object findPasswordByPage(String userId) {
+        System.out.println(userId);
+        Object passwordList = bizPasswordService.selectList(null);
+        return passwordList;
+    }
+
+    @ApiOperation("获取账单明细")
+    @PostMapping("/getBillingDetail")
+    public JSONResult getBillingDetail(BillingDetailVo vo) {
+        JSONResult result = new JSONResult();
+        if(vo == null){
+            return new JSONResult(JSONResult.CODE_FAIL,"参数错误，vo为空！");
+        }
+        List<BillingDetail> billingDetailList = Lists.newArrayList();
+        if(Objects.nonNull(vo.getUserId())){
+            // 查询关于该用户的所有账单
+            Wrapper<BillingDetail> wrapper = new EntityWrapper<>();
+            wrapper = wrapper.eq("user_id", vo.getUserId());
+            billingDetailList = billingDetailService.selectList(wrapper);
+            result = new JSONResult(JSONResult.CODE_SUCCESS,"获取账单明细成功！",billingDetailList);
+        }else{
+            result =  new JSONResult(JSONResult.CODE_FAIL,"参数错误，userId为空！");
+        }
+        return result;
+
+    }
+
+    @ApiOperation("添加账单明细")
+    @PostMapping("/addBillingDetail")
+    public JSONResult addBillingDetail(BillingDetailVo vo) {
+        JSONResult result;
+        if(vo == null){
+            return new JSONResult(JSONResult.CODE_FAIL,"参数错误，vo为空！");
+        }
+        BillingDetail billingDetail = new BillingDetail();
+        BeanUtils.copyProperties(vo,billingDetail);
+        billingDetailService.insert(billingDetail);
+        result = new JSONResult(JSONResult.CODE_SUCCESS,"添加成功！");
+        return result;
+
     }
 
 }
--- guns-admin/src/main/java/com/stylefeng/guns/modular/api/RestBillingDetailController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/api/RestBillingDetailController.java
@@ -1,78 +1,78 @@
-package com.stylefeng.guns.modular.api;
-
-import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.baomidou.mybatisplus.mapper.Wrapper;
-import com.google.common.collect.Lists;
-import com.stylefeng.guns.core.base.controller.BaseController;
-import com.stylefeng.guns.core.common.model.JSONResult;
-import com.stylefeng.guns.core.log.LogObjectHolder;
-import com.stylefeng.guns.core.shiro.ShiroKit;
-import com.stylefeng.guns.modular.billingDetail.service.IBillingDetailService;
-import com.stylefeng.guns.modular.billingDetail.vo.BillingDetailVo;
-import com.stylefeng.guns.modular.system.model.BillingDetail;
-import com.stylefeng.guns.modular.system.model.Customer;
-import com.stylefeng.guns.modular.system.model.Dept;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
-
-/**
- * 账单明细控制器
- *
- * @author fengshuonan
- * @Date 2018-12-21 11:01:19
- */
-@Api(description = "账单明细接口")
-@RestController
-@RequestMapping("/restBillingDetail")
-public class RestBillingDetailController extends BaseController {
-
-    @Autowired
-    private IBillingDetailService billingDetailService;
-
-    @ApiOperation("获取账单明细")
-    @PostMapping("/getBillingDetail")
-    public JSONResult getBillingDetail(BillingDetailVo vo) {
-        JSONResult result = new JSONResult();
-        if(vo == null){
-            return new JSONResult(JSONResult.CODE_FAIL,"参数错误，vo为空！");
-        }
-        List<BillingDetail> billingDetailList = Lists.newArrayList();
-        if(Objects.nonNull(vo.getUserId())){
-            // 查询关于该用户的所有账单
-            Wrapper<BillingDetail> wrapper = new EntityWrapper<>();
-            wrapper = wrapper.eq("user_id", vo.getUserId());
-            billingDetailList = billingDetailService.selectList(wrapper);
-            result = new JSONResult(JSONResult.CODE_SUCCESS,"获取账单明细成功！",billingDetailList);
-        }else{
-            result =  new JSONResult(JSONResult.CODE_FAIL,"参数错误，userId为空！");
-        }
-        return result;
-
-    }
-
-    @ApiOperation("添加账单明细")
-    @PostMapping("/addBillingDetail")
-    public JSONResult addBillingDetail(BillingDetailVo vo) {
-        JSONResult result;
-        if(vo == null){
-            return new JSONResult(JSONResult.CODE_FAIL,"参数错误，vo为空！");
-        }
-        BillingDetail billingDetail = new BillingDetail();
-        BeanUtils.copyProperties(vo,billingDetail);
-        billingDetailService.insert(billingDetail);
-        result = new JSONResult(JSONResult.CODE_SUCCESS,"添加成功！");
-        return result;
-
-    }
-
-}
+//package com.stylefeng.guns.modular.api;
+//
+//import com.baomidou.mybatisplus.mapper.EntityWrapper;
+//import com.baomidou.mybatisplus.mapper.Wrapper;
+//import com.google.common.collect.Lists;
+//import com.stylefeng.guns.core.base.controller.BaseController;
+//import com.stylefeng.guns.core.common.model.JSONResult;
+//import com.stylefeng.guns.core.log.LogObjectHolder;
+//import com.stylefeng.guns.core.shiro.ShiroKit;
+//import com.stylefeng.guns.modular.billingDetail.service.IBillingDetailService;
+//import com.stylefeng.guns.modular.billingDetail.vo.BillingDetailVo;
+//import com.stylefeng.guns.modular.system.model.BillingDetail;
+//import com.stylefeng.guns.modular.system.model.Customer;
+//import com.stylefeng.guns.modular.system.model.Dept;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import org.apache.commons.lang.StringUtils;
+//import org.springframework.beans.BeanUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.Date;
+//import java.util.List;
+//import java.util.Objects;
+//
+///**
+// * 账单明细控制器
+// *
+// * @author fengshuonan
+// * @Date 2018-12-21 11:01:19
+// */
+//@Api(description = "账单明细接口")
+//@RestController
+//@RequestMapping("/restBillingDetail")
+//public class RestBillingDetailController extends BaseController {
+//
+//    @Autowired
+//    private IBillingDetailService billingDetailService;
+//
+//    @ApiOperation("获取账单明细")
+//    @PostMapping("/getBillingDetail")
+//    public JSONResult getBillingDetail(BillingDetailVo vo) {
+//        JSONResult result = new JSONResult();
+//        if(vo == null){
+//            return new JSONResult(JSONResult.CODE_FAIL,"参数错误，vo为空！");
+//        }
+//        List<BillingDetail> billingDetailList = Lists.newArrayList();
+//        if(Objects.nonNull(vo.getUserId())){
+//            // 查询关于该用户的所有账单
+//            Wrapper<BillingDetail> wrapper = new EntityWrapper<>();
+//            wrapper = wrapper.eq("user_id", vo.getUserId());
+//            billingDetailList = billingDetailService.selectList(wrapper);
+//            result = new JSONResult(JSONResult.CODE_SUCCESS,"获取账单明细成功！",billingDetailList);
+//        }else{
+//            result =  new JSONResult(JSONResult.CODE_FAIL,"参数错误，userId为空！");
+//        }
+//        return result;
+//
+//    }
+//
+//    @ApiOperation("添加账单明细")
+//    @PostMapping("/addBillingDetail")
+//    public JSONResult addBillingDetail(BillingDetailVo vo) {
+//        JSONResult result;
+//        if(vo == null){
+//            return new JSONResult(JSONResult.CODE_FAIL,"参数错误，vo为空！");
+//        }
+//        BillingDetail billingDetail = new BillingDetail();
+//        BeanUtils.copyProperties(vo,billingDetail);
+//        billingDetailService.insert(billingDetail);
+//        result = new JSONResult(JSONResult.CODE_SUCCESS,"添加成功！");
+//        return result;
+//
+//    }
+//
+//}
